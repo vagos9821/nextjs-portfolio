a40c7cd4f0f9b579b860f33c487aa9dbe8713346
Vangelis Sigalas <yourtube12321@gmail.com>
2024-02-22 18:14:08 +0200

    fix connect

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -64,10 +64,10 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base md:text-lg ">
-            I am a full stack web developer with a passion for creating
+            I am a front end web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
+            working with JavaScript, React, Redux, Node.js, Python, NextJS,
+            Tailwind, HTML, CSS, and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
           </p>
