90fedf5294583f7ea55e741f478b18f57d5db59a
Vangelis Sigalas <yourtube12321@gmail.com>
2024-02-22 17:46:39 +0200

    added link to cv

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -48,7 +48,15 @@ const HeroSection = () => {
             >
               Hire Me
             </Link>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-red-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
+            <button
+              onClick={() => {
+                const url = "https://lavender-wanids-69.tiiny.site/";
+                if (typeof window !== "undefined") {
+                  window.location.href = url;
+                }
+              }}
+              className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-red-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
